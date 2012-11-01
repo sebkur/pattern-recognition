1d104a898be99e929bae8b32f10600b572ea9b33
Lutz Freitag <lutz.freitag@fu-berlin.de>
2012-11-01 20:36:43 +0100

    removed my stuff... that was double work

--- matlab/Zettel2/separateSample.m
+++
@@ -1,4 +0,0 @@
-function [vec, class] = separateSample(sample)
-	vec = sample(1:end-1);
-	class = sample(end);
-end
--- matlab/Zettel2/splitVector.m
+++
@@ -1,8 +0,0 @@
-function [x, y] = splitVector(data)
-	w = length(data);
-	x = []; y = [];
-	for i = 1:2:(w - 1)
-		x = [x , data(i)];
-		y = [y , data(i + 1)];
-	end
-end
