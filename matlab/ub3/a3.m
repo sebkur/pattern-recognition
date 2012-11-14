452de2e3e4e099309e881b9029cbed1a464c133f
Lutz Freitag <lutz.freitag@fu-berlin.de>
2012-11-14 18:23:03 +0100

    image creating version

--- matlab/ub3/a3.m
+++ matlab/ub3/a3.m
@@ -4,6 +4,26 @@ y = x = linspace(-5, 5, 50);
 
 [xx, yy] = meshgrid(y, x);
 
+
+
+fb = e .^ (- ((xx - 0.5) .^ 2 - (xx - 0.5) .* (yy + 0.5) + (yy + 0.5) .^ 2) ./ (3 / 4))
+
+surfc(xx, yy, fb)
+pause;
+print("3a2.png");
+
+fbax = (1 - 8 / 3 .* xx - 4 / 3 .* yy) .* e .^ ( -(1 .- xx .+ 4 ./ 3 .* xx .^ 2  .- 4 ./ 3 .* xx .* yy .+ yy .+ 4/3 .* yy .^2))
+fbay = -(1 - 8 / 3 .* yy - 4 / 3 .* xx) .* e .^ ( -(1 .- xx .+ 4 ./ 3 .* xx .^ 2  .- 4 ./ 3 .* xx .* yy .+ yy .+ 4/3 .* yy .^2))
+
+
+surfc(xx, yy, fbax)
+pause;
+print("3c2x.png");
+surfc(xx, yy, fbay)
+pause;
+print("3c2x.png");
+
+
 fa  = - (xx .^ 2 + yy .^ 2) *4;
 
 faax = -(8 * xx);
@@ -11,16 +31,13 @@ faay = -(8 * yy);
 
 surfc(xx, yy, fa)
 pause;
+print("3a1.png");
 
 surfc(xx, yy, faax)
 pause;
+print("3c1x.png");
 surfc(xx, yy, faay)
 pause;
+print("3c1y.png");
 
-exit;
 
-fb = e .^ (- ((xx - 0.5) .^ 2 - (xx - 0.5) .* (yy + 0.5) + (yy + 0.5) .^ 2) ./ (3 / 4))
-
-surfc(xx, yy, fb)
-
-pause;
