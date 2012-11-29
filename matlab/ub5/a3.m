da4fd2c7700d16bb10d4ad7bed4172bf27286a42
Lutz Freitag <lutz.freitag@fu-berlin.de>
2012-11-29 15:12:59 +0100

    removed those merge files

--- matlab/ub5/a3.m
+++ matlab/ub5/a3.m
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 % lade Daten
 trainingData = load("-ascii", "pendigits-training.txt");
 
@@ -26,33 +25,3 @@ for i = 1:100
 end
 
 t(:,1) .- x
-=======
-% Daten laden
-trainingData = load("-ascii", "pendigits-training.txt");
-% wir arbeiten mit der Acht
-digit = 8;
-
-% select all samples labeled with 'digit'
-samples = trainingData(trainingData(:,17) == digit,:)(:,1:end - 1);
-
-% compute mu and covanriance matrix
-[mu, cov] = gauss(samples);
-
-% plot the mean vector
-plotDigit(mu, int2str(digit), 'mean.pdf', 'mean.png')
-
-% select a random vector as x0
-x0 = normalize(rand(1, 16)');
-% start with xk = x0
-xk = x0;
-for k = 0:14
-	clf;
-	% create a plot of the iteration
-	label = sprintf('iteration %d', k);
-	pdfname = sprintf('iteration%d.pdf', k);
-	pngname = sprintf('iteration%d.png', k);
-	plotDigit(xk, label, pdfname, pngname);
-	% multiply xk with the covariance matrix
-	xk = normalize(cov * xk);
-end
->>>>>>> 3cf852a9da54141e02777119287189b19300391e
