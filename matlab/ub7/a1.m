a071d8799e83bd3c0be06896cc4c54ab0aae09e4
Lutz Freitag <lutz.freitag@fu-berlin.de>
2012-12-11 19:17:10 +0100

    erste Teilaufgabe wuuhuuu

---
+++ matlab/ub7/a1.m
@@ -0,0 +1,57 @@
+
+function [s ds] = sigmoid(val)
+	s = 1 ./ (1 .+ e.^(-val));
+	ds = s .* (1 .- s);
+end
+
+inputs  = [	[0, 0, 0]; ...
+			[0, 1, 1]; ...
+			[1, 0, 1]; ...
+			[1, 1, 0]]
+
+W1 = rand(3,2);
+W2 = rand(3,1);
+
+gamma = 0.04;
+
+error = inf
+errorOverTime = [];
+while (error > 0.001)
+	error = 0;
+	correction1 = zeros(size(W1));
+	correction2 = zeros(size(W2));
+	for input = inputs'
+		% seperate the input from expected output
+		output = input(end);
+		input = augmentWithOnes(input(1:end - 1)')';
+		
+		% forward propagation 
+		o1 = sigmoid(input' * W1);
+		o2 = sigmoid(augmentWithOnes(o1) * W2);
+		
+		e = (o2 .- output);
+		error += e;
+		
+		D1 = diag((o1 .* (1 .- o1)));
+		D2 = diag((o2 .* (1 .- o2)));
+		
+		% the small W2 matrix
+		W2s = W2(1:end - 1,:);
+		
+		delta2 = D2 * e;
+		delta1 = D1 * W2s * delta2;
+		
+		correction1 += (- gamma * delta1 * input')';
+		correction2 += (- gamma * delta2 * augmentWithOnes(o1))';
+	end
+	W1 += correction1;
+	W2 += correction2;
+	error
+	errorOverTime = [errorOverTime, error];
+end
+
+W1
+W2
+
+plot(errorOverTime);
+pause
