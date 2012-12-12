a29564767da789aec0d8f1b1c6efbaefa0d0de88
Lutz Freitag <lutz.freitag@fu-berlin.de>
2012-12-12 10:30:21 +0100

    habs mal repariert... war noch ein kleiner Fehler drin

--- matlab/ub7/a1.m
+++ matlab/ub7/a1.m
@@ -1,6 +1,6 @@
 
 function [s ds] = sigmoid(val)
-	s = 1 ./ (1 .+ e.^(-val));
+	s = 1 ./ (1 .+ e.^(-val * 2));
 	ds = s .* (1 .- s);
 end
 
@@ -12,14 +12,15 @@ inputs  = [	[0, 0, 0]; ...
 W1 = rand(3,2);
 W2 = rand(3,1);
 
-gamma = 0.04;
+gamma = 0.1;
 
 error = inf
 errorOverTime = [];
-while (error > 0.001)
+while (error > 0.01)
 	error = 0;
 	correction1 = zeros(size(W1));
 	correction2 = zeros(size(W2));
+	
 	for input = inputs'
 		% seperate the input from expected output
 		output = input(end);
@@ -30,20 +31,21 @@ while (error > 0.001)
 		o2 = sigmoid(augmentWithOnes(o1) * W2);
 		
 		e = (o2 .- output);
-		error += e;
+		error += 0.5 * e^2;
 		
 		D1 = diag((o1 .* (1 .- o1)));
 		D2 = diag((o2 .* (1 .- o2)));
 		
 		% the small W2 matrix
-		W2s = W2(1:end - 1,:);
+		W2s = W2(2:end,:);
 		
 		delta2 = D2 * e;
 		delta1 = D1 * W2s * delta2;
 		
-		correction1 += (- gamma * delta1 * input')';
-		correction2 += (- gamma * delta2 * augmentWithOnes(o1))';
+		correction1 -= (gamma * delta1 * input')';
+		correction2 -= (gamma * delta2 * augmentWithOnes(o1))';
 	end
+	
 	W1 += correction1;
 	W2 += correction2;
 	error
@@ -52,6 +54,15 @@ end
 
 W1
 W2
+
+for input = inputs'
+	% seperate the input from expected output
+	output = input(end)
+	input = augmentWithOnes(input(1:end - 1)')'
+	o1 = sigmoid(input' * W1);
+	o2 = sigmoid(augmentWithOnes(o1) * W2)
+	
+end
 
 plot(errorOverTime);
 pause
