d444dede17efdbd32d6abd27a2cdb946a4e18ac9
Sebastian Kürten <sebastian.kuerten@fu-berlin.de>
2013-01-05 20:39:40 +0100

    Kommentare

--- matlab/ub9/hessian_classify.m
+++ matlab/ub9/hessian_classify.m
@@ -1,7 +1,12 @@
+% Klassifizieren alle Daten in 'features'. Trenne linear in class1 und class2
 function predictions = hessian_classify(theta, r, class1, class2, features)
+	% normale zu der Geradengleichung
 	normal = [cos(theta); sin(theta)];
+	% projiziere alle daten auf die Normale
 	projections = (features*normal) * normal';
+	% bestimme die Länge der Projektion
 	norms = norm(projections, OPT='rows');
+	% klassifiziere durch Vergleich mit dem Radius
 	class1idx = find(norms < r);
 	class2idx = find(1-(norms < r));
 	predictions = zeros(size(features,1), 1);
